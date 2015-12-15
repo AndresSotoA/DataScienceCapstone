96057c4b1e3f2046625d753dd10708a66314b901
jiahsuanlo <jiahsuanlo@yahoo.com>
2015-12-14 22:26:45 -0500

    Add n-gram hash generation codes

--- task2_exploratory.R
+++ task2_exploratory.R
@@ -1,6 +1,6 @@
 library(hash)
 
-buildFreqHash<- function(wordVector)
+build1GramHash<- function(wordVector)
 {
     freqHT<- hash()
     for (wd in wordVector)
@@ -14,3 +14,41 @@ buildFreqHash<- function(wordVector)
     # return hash table
     freqHT
 }
+
+# n-gram hash table generation
+# wordVector: a vector of words
+# n: find n-gram
+# return n-gram hash table
+buildNGramHash<- function(wordVector,n)
+{
+    freqHT<- hash()
+    wdPrev<- ""
+    ct<- 0
+    for (wd in wordVector)
+    {
+        ct<- ct+1
+        if (ct< n)
+        {
+            wdPrev<- paste(wdPrev,wd)
+            # remove first space if any
+            wdPrev<- gsub("^(\\s)(.*)$","\\2",wdPrev)
+            next
+        }
+        # build word pair
+        nGram<- paste(wdPrev,wd)
+        # remove first space if any
+        nGram<- gsub("^(\\s)(.*)$","\\2",nGram)
+        
+        # put in hash
+        if (has.key(nGram,freqHT))
+            freqHT[[nGram]]<- freqHT[[nGram]]+1
+        else
+            freqHT[[nGram]]<- 1
+        
+        # update previous wd
+        wdPrev<- gsub("^(.*)\\s(.*)$","\\2",nGram)
+    }
+    # return hash
+    freqHT
+}
+
