f8b8ffafdc0fbbc4806912f0cc18c2b8cc6277cc
jiahsuanlo <joshloqs@gmail.com>
2015-12-24 13:37:43 -0500

    add VocabularyCoverage function to find the ratio (number of words/vocabulary) that can achieve x-% coverage

--- UtilityFunctions.R
+++ UtilityFunctions.R
@@ -43,3 +43,47 @@ striatifiedSelectLines<- function(lines, nSample=1e3)
     lines[ivec]
     
 }
+
+# obtain xx% coverage
+# input coverage percentage and return the the ratio
+# (number of words/vocabulary) that 
+# can achieve this coverage
+VocabularyCoverage<- function(ngram, coverage= 90)
+{
+    ngs<- sort(ngram, decreasing = TRUE)
+    
+    tol = 0.01 # 1% tolerance
+    # obtain 90% coverage vocabulary ratio
+    cntUB<- length(ngs)
+    cntLB<- 1
+    iter<- 1
+    while (TRUE)
+    {
+        iter<- iter+1 
+        
+        cnt<- cntLB+ round((cntUB-cntLB)/2)
+        cover<- sum(ngs[1:cnt])/sum(ngs)
+        if (cover > coverage/100+tol)
+        {
+            cntUB<- cnt
+            
+        }
+        else if (cover < coverage/100-tol)
+        {
+            cntLB<- cnt
+        }
+        else
+        {
+            CoverageVocRatio<-  cnt/length(ngs)
+            break
+        }
+        
+        if (iter> 1e3)
+        {
+            CoverageVocRatio<-  cnt/length(ngs)
+            break
+        }
+    }
+    # return 
+    CoverageVocRatio
+}
